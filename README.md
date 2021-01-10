d77c106ba570f6d835d4ddfa5d0cfacae49bc08f
JericoFX <56100605+JericoFX@users.noreply.github.com>
2021-01-10 01:23:43 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# carremote
+
+## Description
+Car Remote translated to QB by JericoFX
+
+## Version
+1.0.0
+
+## Requirements
+* QB or similar
+* Rename the script to carremote
+* Change the Core String in Config.Core -- Like QBCore:GetObject or similar
+* Dont worry im not trying to steal yours scripts, thats is impossible, only in a crazy fuck mind like xDope. big HI to this "developer".
+*
+## Installation
+1) Unzip download to the `resources/carremote` directory.
+
+2) Add `start carremote` in your server.cfg.
---
+++ locales/en.lua
@@ -0,0 +1,11 @@
+Locales['en'] = {
+  ['now_connected'] = 'Your key is connected to the car.',
+  ['unlocked'] = 'The vehicle is unlocked.',
+  ['locked'] = 'The vehicle is locked.',
+  ['engine_on'] = 'The engine of the vehicle is turned on.',
+  ['engine_off'] = 'Vehicle engine shut down.',
+  ['already_locked'] = 'The vehicle is already locked.',
+  ['already_unlocked'] = 'The car is already unlocked.',
+  ['out_of_range'] = 'You are out of range of your car.',
+  ['not_connected'] = 'Your key is not linked to any vehicle.'
+}
